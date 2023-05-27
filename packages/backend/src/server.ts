83ef0249bbb8e1392756381d866e7dfe14beb284
Ballistc-dot <pedrocdn2014@gmail.com>
2023-05-27 18:57:01 -0300

    add node_modules to git ignore

---
+++ packages/backend/src/server.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+import { routes } from './routes'
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.register(routes)
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
