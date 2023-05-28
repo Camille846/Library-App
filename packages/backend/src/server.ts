eb30754fdccecc7da51ded97615c3f21a9d816c1
Ballistc-dot <pedrocdn2014@gmail.com>
2023-05-27 22:31:31 -0300

    setup husky + lint-staged

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -10,7 +10,7 @@ fastify.register(routes)
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
     fastify.log.error(err)
-    
+
     process.exit(1)
   }
   // Server is now listening on ${address}
